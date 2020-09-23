3b8fd354e81af44cf2430f9dbe2b39456f61ca25
K McNaught <kirsty.mcnaught@gmail.com>
2020-09-23 21:19:00 +0100

    Add logging

--- src/JuliusSweetland.OptiKey.Core/DataFilters/KalmanFilter.cs
+++ src/JuliusSweetland.OptiKey.Core/DataFilters/KalmanFilter.cs
@@ -1,4 +1,7 @@
 ﻿// Copyright (c) 2020 OPTIKEY LTD (UK company number 11854839) - All Rights Reserved
+
+using log4net;
+
 namespace JuliusSweetland.OptiKey.DataFilters
 {
     public class KalmanFilter
@@ -8,6 +11,9 @@ namespace JuliusSweetland.OptiKey.DataFilters
         double EstimationConfidence; //P
         double EstimatedValue; // X 
         double Gain; // K
+
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
 
         public KalmanFilter(double initialValue = 0f, double confidenceOfInitialValue = 1f, double processNoise = 0.0001f, double measurementNoise = 0.01f)
         {
@@ -23,6 +29,12 @@ namespace JuliusSweetland.OptiKey.DataFilters
             EstimationConfidence = MeasurementNoise * (EstimationConfidence + ProcessNoise) / (MeasurementNoise + EstimationConfidence + ProcessNoise);
             double result = EstimatedValue + (measurement - EstimatedValue) * Gain;
             EstimatedValue = result;
+
+            Log.InfoFormat("{0} Measurement: {1}", this.GetHashCode(), measurement);
+            Log.InfoFormat("{0} Prediction: {1}", this.GetHashCode(), EstimatedValue);
+            Log.InfoFormat("{0} Uncertainty: {1}", this.GetHashCode(), EstimationConfidence);
+            Log.InfoFormat("{0} Gain: {1}", this.GetHashCode(), Gain);
+
             return result;
         }
     }
