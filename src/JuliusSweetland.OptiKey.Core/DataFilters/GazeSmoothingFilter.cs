60594f56265e7a4841dd7991f2272a8b2963e6b0
K McNaught <kirsty.mcnaught@gmail.com>
2020-09-30 16:45:50 +0100

    Tuning and lots of comments

--- src/JuliusSweetland.OptiKey.Core/DataFilters/GazeSmoothingFilter.cs
+++ src/JuliusSweetland.OptiKey.Core/DataFilters/GazeSmoothingFilter.cs
@@ -2,6 +2,7 @@
 using JuliusSweetland.OptiKey.Properties;
 using System;
 using System.Windows;
+using JuliusSweetland.OptiKey.Enums;
 using log4net;
 
 namespace JuliusSweetland.OptiKey.DataFilters
@@ -46,38 +47,43 @@ namespace JuliusSweetland.OptiKey.DataFilters
 
             // dictates scale across all deltas
             // lower = more smoothness (mainly noticeable at fixation)
-            var processScale = 0.01d;
+            // This was was too high (1.0) in my first expKF which was responsible for a lot of the floatiness
+            var processScale = 0.005d; 
 
             // dictates how quickly PN scales up with delta
             // higher = extends smoothness for longer saccades
-            var processIncreaseScaleFactor = 10.0d; 
+            var processIncreaseScaleFactor = 5.0d * (int)Settings.Default.GazeSmoothingLevel;
 
-            // extends the "fixation zone" for extra smoothness without affecting later ramp-up (this keeps PN > 0.0)
-            var processDeadzoneOffset = 20.0;
+            // shifts our exponential to squash the curve at the "fixation zone" for extra smoothness
+            var processDeadzoneOffset = 20.0d * (int)Settings.Default.GazeSmoothingLevel - 10.0f;
 
             // alternatively extends the "fixation zone" with a subtraction - this supports a region of zero process noise, i.e. a hard deadzone
             var processDeadzone = 0.0;
-
-            // ======================================================== //
-
+            
+            // =========================================================================== //
+            // Process model: this encodes all our desired behaviour wrt smoothness 
+            // Feel free to change the mapping if appropriate, but ideally stick to tuning the variables above
             var delta = (measuredPoint - EstimatedPoint).Length;
-            //var currentProcessNoise = Math.Exp(delta/processIncreaseScaleFactor);
             var currentProcessNoise = processScale * Math.Exp((delta - processDeadzoneOffset) / processIncreaseScaleFactor) - processDeadzone;
 
+            // The process deadzone part allows a region with zero process noise - I personally prefer to just squash the noise to "really low" so we
+            // never get 100% "stuck". You can achieve this instead with processScale and processDeadzoneOffset instead. (i.e. I'd prefer we lose this bit)
             if (processDeadzone > 0.0)
             {
                 currentProcessNoise -= Math.Log(processDeadzone);
                 currentProcessNoise = Math.Max(0.0, currentProcessNoise); // force non-negative
             }
-            
-            // process noise can be very low when fixating since data comes in v fast
 
+            // == End of process model ==================================================== //
+            
+            // == Standard update equations from the Kalman Filter framework - these shouldn't be changed == //
             EstimationNoise = EstimationNoise + currentProcessNoise;
 
             // Update
             Gain = (EstimationNoise) / (EstimationNoise + MeasurementNoise);
             EstimationNoise = (1.0 - Gain) * EstimationNoise;
 
+            // Logs for visualisation, to be removed
             Log.InfoFormat("{0} Measurement: {1}", this.GetHashCode(), measuredPoint);
             Log.InfoFormat("{0} Prediction: {1}", this.GetHashCode(), EstimatedPoint);
             Log.InfoFormat("{0} Uncertainty: {1}", this.GetHashCode(), EstimationNoise);
