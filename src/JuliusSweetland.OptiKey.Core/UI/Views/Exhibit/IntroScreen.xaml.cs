3de5a695e1c9faa3cf086580dc56fc84590e8b56
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2021-06-22 09:32:47 +0100

    Add WIP intro screen

---
+++ src/JuliusSweetland.OptiKey.Core/UI/Views/Exhibit/IntroScreen.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace JuliusSweetland.OptiKey.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for IntroScreen.xaml
+    /// </summary>
+    public partial class IntroScreen : Window
+    {
+        public IntroScreen()
+        {
+            InitializeComponent();
+
+            var fadeInOutAnimation = new DoubleAnimation
+            {
+                From = 0.25,
+                To = 0.5,
+                Duration = TimeSpan.FromSeconds(0.25),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever,
+            };
+
+            rectangle.BeginAnimation(OpacityProperty, fadeInOutAnimation);
+        }        
+    }
+}
