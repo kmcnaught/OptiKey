7cd6fca140933bc778382afefce1c2737a841956
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2021-06-22 09:33:03 +0100

    Add WIP post-calibration window

---
+++ src/JuliusSweetland.OptiKey.Core/UI/Views/Exhibit/PostCalib.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+
+namespace JuliusSweetland.OptiKey.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for PostCalib.xaml
+    /// </summary>
+    public partial class PostCalib : Window
+    {
+        public PostCalib()
+        {
+            InitializeComponent();
+
+            var fadeInOutAnimation = new DoubleAnimation
+            {
+                From = 0.25,
+                To = 0.5,
+                Duration = TimeSpan.FromSeconds(0.25),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever,
+            };
+
+            rectangle.BeginAnimation(OpacityProperty, fadeInOutAnimation);
+            rectangle2.BeginAnimation(OpacityProperty, fadeInOutAnimation);
+        }        
+    }
+}
