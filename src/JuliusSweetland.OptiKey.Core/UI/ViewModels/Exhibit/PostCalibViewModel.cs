1da69f21f43f35808ad226f8553d1eb09eb7c635
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2021-06-23 13:07:57 +0100

    Temporary "waiting for calib" message

--- src/JuliusSweetland.OptiKey.Core/UI/ViewModels/Exhibit/PostCalibViewModel.cs
+++ src/JuliusSweetland.OptiKey.Core/UI/ViewModels/Exhibit/PostCalibViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using EyeXFramework;
+using JuliusSweetland.OptiKey.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tobii.EyeX.Framework;
 
 namespace JuliusSweetland.OptiKey.UI.ViewModels.Exhibit
 {
@@ -16,10 +19,40 @@ namespace JuliusSweetland.OptiKey.UI.ViewModels.Exhibit
 
         public override void SetUp()
         {
+            SetInitTime();
+
+            // Sign up for "tracking status changed" event - this is triggered at end of calibration 
+            // (regardless of success or failure..)            
+            TobiiEyeXPointService.EyeXHost.EyeTrackingDeviceStatusChanged += handleTobiiChange;
+        }
+
+        private void handleTobiiChange(object sender, EngineStateValue<EyeTrackingDeviceStatus> status)
+        {
+            if (status.Value == EyeTrackingDeviceStatus.Tracking)
+            {
+                waitingForCalibration = false;
+            }
+            else
+            {
+                waitingForCalibration = true;
+            }
+            RaisePropertyChanged("WaitingForCalibration");
         }
 
         public override void TearDown()
+        {            
+            waitingForCalibration = false;
+            RaisePropertyChanged("WaitingForCalibration");
+
+            TobiiEyeXPointService.EyeXHost.EyeTrackingDeviceStatusChanged -= handleTobiiChange;
+        }
+
+
+        private bool waitingForCalibration = true;
+        public bool WaitingForCalibration
         {
+            get { return waitingForCalibration; }
+            set { SetProperty(ref waitingForCalibration, value); }
         }
     }
 }
