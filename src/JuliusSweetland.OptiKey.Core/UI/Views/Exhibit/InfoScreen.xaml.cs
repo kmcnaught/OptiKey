410a4e250e73edc281b5172febcac17e747ef8ce
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2021-06-28 15:41:32 +0100

    New views

---
+++ src/JuliusSweetland.OptiKey.Core/UI/Views/Exhibit/InfoScreen.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace JuliusSweetland.OptiKey.UI.Views.Exhibit
+{
+    /// <summary>
+    /// Interaction logic for IntroScreen.xaml
+    /// </summary>
+    public partial class InfoScreen : UserControl
+    {
+        public InfoScreen()
+        {
+            InitializeComponent();
+
+            var fadeInOutAnimation = new DoubleAnimation
+            {
+                From = 0.25,
+                To = 0.5,
+                Duration = TimeSpan.FromSeconds(0.25),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever,
+            };
+
+            rectangle.BeginAnimation(OpacityProperty, fadeInOutAnimation);
+        }        
+    }
+}
---
+++ src/JuliusSweetland.OptiKey.Core/UI/Views/Exhibit/ResetScreen.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace JuliusSweetland.OptiKey.UI.Views.Exhibit
+{
+    /// <summary>
+    /// Interaction logic for ResetScreen.xaml
+    /// </summary>
+    public partial class ResetScreen : UserControl
+    {
+        public ResetScreen()
+        {
+            InitializeComponent();
+
+            var fadeInOutAnimation = new DoubleAnimation
+            {
+                From = 0.25,
+                To = 0.5,
+                Duration = TimeSpan.FromSeconds(0.25),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever,
+            };
+
+            rectangle.BeginAnimation(OpacityProperty, fadeInOutAnimation);
+        }        
+    }
+}
