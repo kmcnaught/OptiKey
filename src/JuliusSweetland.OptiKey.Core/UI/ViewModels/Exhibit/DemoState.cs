8592a95f979c52d2ceb8c0c12024094f7973628b
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2021-06-28 16:31:53 +0100

    Reinstate Ctrl modifier for keys

--- src/JuliusSweetland.OptiKey.Core/UI/ViewModels/Exhibit/DemoState.cs
+++ src/JuliusSweetland.OptiKey.Core/UI/ViewModels/Exhibit/DemoState.cs
@@ -49,10 +49,10 @@ namespace JuliusSweetland.OptiKey.UI.ViewModels.Exhibit
             LaunchOnboarding();
 
             bool noRepeat = true;
-            HotkeyManager.Current.AddOrReplace("Back", Key.Left, ModifierKeys.None, noRepeat, OnBack);
-            HotkeyManager.Current.AddOrReplace("Forward", Key.Right, ModifierKeys.None, noRepeat, OnForward);
-            HotkeyManager.Current.AddOrReplace("Reset", Key.Down, ModifierKeys.None, noRepeat, OnReset);
-            HotkeyManager.Current.AddOrReplace("Info", Key.Up, ModifierKeys.None, noRepeat, OnInfo);
+            HotkeyManager.Current.AddOrReplace("Back", Key.Left, ModifierKeys.Control, noRepeat, OnBack);
+            HotkeyManager.Current.AddOrReplace("Forward", Key.Right, ModifierKeys.Control, noRepeat, OnForward);
+            HotkeyManager.Current.AddOrReplace("Reset", Key.Down, ModifierKeys.Control, noRepeat, OnReset);
+            HotkeyManager.Current.AddOrReplace("Info", Key.Up, ModifierKeys.Control, noRepeat, OnInfo);
 
             // Launch Minecraft
             string javapath = @"C:\Program Files (x86)\Minecraft Launcher\runtime\jre-legacy\windows-x64\jre-legacy\bin\javaw.exe";
