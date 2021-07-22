f29b069863fe9d6fb14cb4674d2dc971ab8e4da5
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2021-07-22 11:43:21 +0100

    Break out common UI elements

---
+++ src/JuliusSweetland.OptiKey.Core/UI/Views/Exhibit/UserControls/ButtonBack.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JuliusSweetland.OptiKey.UI.Views.Exhibit.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ButtonBack.xaml
+    /// </summary>
+    public partial class ButtonBack : UserControl
+    {
+        public ButtonBack()
+        {
+            InitializeComponent();
+        }
+    }
+}
