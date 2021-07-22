b54c8986f7009f27e80ed692f96391c824eea9fd
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2021-07-22 23:39:24 +0100

    Add new views/VMs

---
+++ src/JuliusSweetland.OptiKey.Core/UI/ViewModels/Exhibit/ErrorViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace JuliusSweetland.OptiKey.UI.ViewModels.Exhibit
+{
+    class ErrorViewModel : PageViewModel
+    {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+
+        public ErrorViewModel()
+        {
+            CanGoBackward = false;
+            CanGoForward = true;
+        }
+
+        public void StartRestartCountdown()
+        {
+            dispatcherTimer.Tick += Restart;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 10);
+            dispatcherTimer.Start();
+        }
+
+        private void Restart(object sender, EventArgs e)
+        {
+            dispatcherTimer.Tick -= Restart;
+            OptiKeyApp.RestartApp();
+        }
+
+        public override void SetUp()
+        {
+            SetInitTime();
+        }
+
+        public override void TearDown()
+        {
+        }
+    }
+}
---
+++ src/JuliusSweetland.OptiKey.Core/UI/ViewModels/Exhibit/LoadingViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuliusSweetland.OptiKey.UI.ViewModels.Exhibit
+{
+    class LoadingViewModel : PageViewModel
+    {
+        public LoadingViewModel()
+        {
+            CanGoBackward = false;
+            CanGoForward = true;
+        }
+
+        public override void SetUp()
+        {
+            SetInitTime();
+        }
+
+        public override void TearDown()
+        {
+        }
+    }
+}
