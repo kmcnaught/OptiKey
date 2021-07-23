8089b5456506c2a646d3e83c051da582080d0751
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2021-07-23 13:39:07 +0100

    Refactor tobii point source to retry setup
    It now recovers from engine being off at start, and
    eye tracking being unplugged.

--- src/JuliusSweetland.OptiKey.Core/Services/TobiiEyeXPointService.cs
+++ src/JuliusSweetland.OptiKey.Core/Services/TobiiEyeXPointService.cs
@@ -23,6 +23,9 @@ namespace JuliusSweetland.OptiKey.Services
 
         private event EventHandler<Timestamped<Point>> pointEvent;
 
+        public event EventHandler<EyeTrackingDeviceStatus> tobiiError;
+
+
         #endregion
 
         #region Ctor
@@ -31,6 +34,7 @@ namespace JuliusSweetland.OptiKey.Services
         {
             KalmanFilterSupported = true;
             EyeXHost = new EyeXHost();
+            StartHost();
 
             //Disconnect (deactivate) from the EyeXHost on shutdown - otherwise the process can hang
             Application.Current.Exit += (sender, args) =>
@@ -57,76 +61,139 @@ namespace JuliusSweetland.OptiKey.Services
 
         public event EventHandler<Exception> Error;
 
+        private DateTime firstConnectAttempt = new DateTime(0);
+        private DateTime zeroTime = new DateTime(0);
+        private int timeoutSeconds = 3*60; // for first connection
+        private bool haveConnectedSuccessfully = false;
+
+        bool streamActive = false;
+
+        public bool StartHost()
+        {
+            //if (EyeXHost.EyeXAvailability == EyeXAvailability.Running &&
+              //  !EyeXHost.IsStarted)
+            if (!EyeXHost.IsStarted)
+            {
+                EyeXHost.EyeTrackingDeviceStatusChanged += EyeXHost_EyeTrackingDeviceStatusChanged;
+
+                EyeXHost.Start(); // Start the EyeX host
+                return true;
+            }
+            else return EyeXHost.IsStarted;
+        }
+
+        private bool SetupEyeXStreams()
+        {
+            Log.Info("Checking the state of the Tobii service...");
+
+            if (firstConnectAttempt == zeroTime)
+            {
+                firstConnectAttempt = DateTime.Now;
+            }
+
+            TimeSpan timeSinceFirstAttempt = DateTime.Now.Subtract(firstConnectAttempt);
+
+            switch (EyeXHost.EyeXAvailability)
+            {
+                case EyeXAvailability.NotAvailable:
+                    PublishError(this, new ApplicationException(Resources.TOBII_EYEX_ENGINE_NOT_FOUND));
+                    return false;
+
+                case EyeXAvailability.NotRunning:
+                    if (!haveConnectedSuccessfully && timeSinceFirstAttempt > new TimeSpan(0, 0, timeoutSeconds))
+                    {
+                        PublishError(this, new ApplicationException(Resources.TOBII_EYEX_ENGINE_NOT_RUNNING));
+                    }
+                    return false;
+
+                case EyeXAvailability.Running:
+                    haveConnectedSuccessfully = true;
+                    break;
+            }
+
+            Log.Info("Attaching eye tracking device status changed listener to the Tobii service.");
+
+            EyeXHost.EyeTrackingDeviceStatusChanged += EyeXHost_EyeTrackingDeviceStatusChanged;
+
+            if (Settings.Default.TobiiEyeXProcessingLevel == DataStreamProcessingLevels.None ||
+               Settings.Default.TobiiEyeXProcessingLevel == DataStreamProcessingLevels.Low)
+            {
+                gazeDataStream = EyeXHost.CreateGazePointDataStream(
+                    Settings.Default.TobiiEyeXProcessingLevel == DataStreamProcessingLevels.None
+                        ? GazePointDataMode.Unfiltered //None
+                        : GazePointDataMode.LightlyFiltered); //Low
+
+                if (!EyeXHost.IsStarted)
+                {
+                    EyeXHost.Start(); // Start the EyeX host
+                }
+
+                gazeDataStream.Next += (s, data) =>
+                {
+                    if (pointEvent != null
+                        && !double.IsNaN(data.X)
+                        && !double.IsNaN(data.Y))
+                    {
+                        pointEvent(this, new Timestamped<Point>(new Point(data.X, data.Y),
+                            new DateTimeOffset(DateTime.UtcNow).ToUniversalTime())); //EyeX does not publish a useable timestamp
+                    }
+                };
+            }
+            else
+            {
+                fixationDataStream = EyeXHost.CreateFixationDataStream(
+                    Settings.Default.TobiiEyeXProcessingLevel == DataStreamProcessingLevels.Medium
+                        ? FixationDataMode.Sensitive //Medium
+                        : FixationDataMode.Slow); //Hight
+
+                if (!EyeXHost.IsStarted)
+                {
+                    EyeXHost.Start(); // Start the EyeX host
+                }
+
+                fixationDataStream.Next += (s, data) =>
+                {
+                    if (pointEvent != null
+                        && !double.IsNaN(data.X)
+                        && !double.IsNaN(data.Y))
+                    {
+                        pointEvent(this, new Timestamped<Point>(new Point(data.X, data.Y),
+                            new DateTimeOffset(DateTime.UtcNow).ToUniversalTime())); //EyeX does not publish a useable timestamp
+                    }
+                };
+            }            
+            return true;
+        }
+
+        private void DisposeEyeXStreams()
+        {
+            if (gazeDataStream != null)
+            {
+                gazeDataStream.Dispose();
+                gazeDataStream = null;
+            }
+
+            if (fixationDataStream != null)
+            {
+                fixationDataStream.Dispose();
+                fixationDataStream = null;
+            }
+        }
+
         public event EventHandler<Timestamped<Point>> Point
         {
             add
             {
-                if (pointEvent == null)
+                if (pointEvent == null) // i.e. no one has subscribed yet?
                 {
-                    Log.Info("Checking the state of the Tobii service...");
-
-                    switch (EyeXHost.EyeXAvailability)
+                    // "NotAvailable" means the library isn't even installed
+                    if (EyeXHost.EyeXAvailability == EyeXAvailability.NotAvailable)
                     {
-                        case EyeXAvailability.NotAvailable:
-                            PublishError(this, new ApplicationException(Resources.TOBII_EYEX_ENGINE_NOT_FOUND));
-                            return;
-
-                        case EyeXAvailability.NotRunning:
-                            PublishError(this, new ApplicationException(Resources.TOBII_EYEX_ENGINE_NOT_RUNNING));
-                            return;
+                        PublishError(this, new ApplicationException(Resources.TOBII_EYEX_ENGINE_NOT_FOUND));
+                        return;
                     }
 
-                    Log.Info("Attaching eye tracking device status changed listener to the Tobii service.");
-
-                    EyeXHost.EyeTrackingDeviceStatusChanged += (s, e) => Log.InfoFormat("Tobii EyeX tracking device status changed to {0} (IsValid={1})", e, e.IsValid);
-
-                    if (Settings.Default.TobiiEyeXProcessingLevel == DataStreamProcessingLevels.None ||
-                       Settings.Default.TobiiEyeXProcessingLevel == DataStreamProcessingLevels.Low)
-                    {
-                        gazeDataStream = EyeXHost.CreateGazePointDataStream(
-                            Settings.Default.TobiiEyeXProcessingLevel == DataStreamProcessingLevels.None
-                                ? GazePointDataMode.Unfiltered //None
-                                : GazePointDataMode.LightlyFiltered); //Low
-
-                        if (!EyeXHost.IsStarted)
-                        {
-                            EyeXHost.Start(); // Start the EyeX host
-                        }
-
-                        gazeDataStream.Next += (s, data) =>
-                        {
-                            if (pointEvent != null
-                                && !double.IsNaN(data.X)
-                                && !double.IsNaN(data.Y))
-                            {
-                                pointEvent(this, new Timestamped<Point>(new Point(data.X, data.Y),
-                                    new DateTimeOffset(DateTime.UtcNow).ToUniversalTime())); //EyeX does not publish a useable timestamp
-                            }
-                        };
-                    }
-                    else
-                    {
-                        fixationDataStream = EyeXHost.CreateFixationDataStream(
-                            Settings.Default.TobiiEyeXProcessingLevel == DataStreamProcessingLevels.Medium
-                                ? FixationDataMode.Sensitive //Medium
-                                : FixationDataMode.Slow); //Hight
-
-                        if(!EyeXHost.IsStarted)
-                        {
-                            EyeXHost.Start(); // Start the EyeX host
-                        }
-
-                        fixationDataStream.Next += (s, data) =>
-                        {
-                            if (pointEvent != null
-                                && !double.IsNaN(data.X)
-                                && !double.IsNaN(data.Y))
-                            {
-                                pointEvent(this, new Timestamped<Point>(new Point(data.X, data.Y),
-                                    new DateTimeOffset(DateTime.UtcNow).ToUniversalTime())); //EyeX does not publish a useable timestamp
-                            }
-                        };
-                    }
+                    //streamActive = SetupEyeXStreams();
                 }
 
                 pointEvent += value;
@@ -139,18 +206,40 @@ namespace JuliusSweetland.OptiKey.Services
                 {
                     Log.Info("Last listener of Point event has unsubscribed. Disposing gaze data & fixation data streams.");
 
-                    if (gazeDataStream != null)
-                    {
-                        gazeDataStream.Dispose();
-                        gazeDataStream = null;
-                    }
-
-                    if (fixationDataStream != null)
-                    {
-                        fixationDataStream.Dispose();
-                        fixationDataStream = null;
-                    }
+                    DisposeEyeXStreams();
                 }
+            }
+        }
+
+        private void EyeXHost_EyeTrackingDeviceStatusChanged(object sender, EngineStateValue<EyeTrackingDeviceStatus> e)
+        {
+            Log.InfoFormat("Tobii EyeX tracking device status changed to {0} (IsValid={1})", e, e.IsValid);
+            Console.WriteLine(e.Value);
+            //can be 0: invalid at first and on shutdown, not sure if this happens at other times
+            switch(e.Value)
+            {
+                case 0:
+                    break;
+                case EyeTrackingDeviceStatus.Initializing:
+                    break;                
+                case EyeTrackingDeviceStatus.InvalidConfiguration:
+                    break;
+                case EyeTrackingDeviceStatus.DeviceNotConnected:
+                    break;
+                case EyeTrackingDeviceStatus.Tracking:
+                    SetupEyeXStreams();
+                    break;
+                case EyeTrackingDeviceStatus.TrackingPaused:
+                    break;
+                case EyeTrackingDeviceStatus.Configuring:
+                    break;
+                case EyeTrackingDeviceStatus.NotAvailable:
+                    DisposeEyeXStreams();
+                    break;
+                case EyeTrackingDeviceStatus.UnknownError:                    
+                case EyeTrackingDeviceStatus.ConnectionError:
+                    tobiiError(this, e.Value);
+                    break;
             }
         }
 
