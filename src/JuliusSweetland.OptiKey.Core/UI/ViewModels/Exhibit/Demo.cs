1f505e153f02c694092f83812a5cd493380f7089
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2021-08-31 11:49:46 +0100

    Remove unused file/code

--- src/JuliusSweetland.OptiKey.Core/UI/ViewModels/Exhibit/Demo.cs
+++ src/JuliusSweetland.OptiKey.Core/UI/ViewModels/Exhibit/Demo.cs
@@ -212,13 +212,7 @@ namespace JuliusSweetland.OptiKey.UI.ViewModels.Exhibit
                 String fullCmd = Settings.Default.MinecraftCommand;
                 if (!String.IsNullOrEmpty(fullCmd))
                 {
-                    string javapath1 = @"C:\Program Files (x86)\Minecraft Launcher\runtime\jre-legacy\windows-x64\jre-legacy\bin\javaw.exe";
-
-                    // args need to come from a previous run of Minecraft with valid credentials
-                    //FIXME: remove unused dupe
-                    String cmdTextFile = AppDomain.CurrentDomain.BaseDirectory + @"\Resources\minecraftCommand.txt";
-                    string minecraftArgs1 = File.ReadAllText(cmdTextFile);
-
+                    string javapath1 = @"C:\Program Files (x86)\Minecraft Launcher\runtime\jre-legacy\windows-x64\jre-legacy\bin\javaw.exe";                   
 
                     string[] parts = fullCmd.SplitAtEndOfSubstring("javaw.exe\"");
                     string javaPath = parts[0];
