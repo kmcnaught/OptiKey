aae72fe5e0e1b049a1e9022a373ec0e5e3f364fe
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2023-02-16 16:02:32 +0000

    Fix "other" tab in settings, and expose new setting

---
+++ src/JuliusSweetland.OptiKey.EyeMine/UI/ViewModels/Management/OtherViewModel.cs
@@ -0,0 +1,115 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using JuliusSweetland.OptiKey.Enums;
+using JuliusSweetland.OptiKey.Models;
+using JuliusSweetland.OptiKey.Properties;
+using JuliusSweetland.OptiKey.Services.PluginEngine;
+using log4net;
+using Prism.Mvvm;
+using JuliusSweetland.OptiKey.Services.Translation.Languages;
+using JuliusSweetland.OptiKey.Services.Translation;
+using System.Windows.Media;
+using System.Linq;
+
+namespace JuliusSweetland.OptiKey.EyeMine.UI.ViewModels.Management
+{
+    public class OtherViewModel : BindableBase
+    {
+        #region Private Member Vars
+
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        #endregion
+
+        #region Ctor
+
+        public OtherViewModel()
+        {
+            Load();
+        }
+
+        #endregion
+
+        #region Properties
+
+        private bool showSplashScreen;
+        public bool ShowSplashScreen
+        {
+            get { return showSplashScreen; }
+            set { SetProperty(ref showSplashScreen, value); }
+        }
+
+        private bool checkForUpdates;
+        public bool CheckForUpdates
+        {
+            get { return checkForUpdates; }
+            set { SetProperty(ref checkForUpdates, value); }
+        }
+
+        private bool suppressTriggerWarning;
+        public bool SuppressTriggerWarning
+        {
+            get { return suppressTriggerWarning; }
+            set { SetProperty(ref suppressTriggerWarning, value); }
+        }
+
+        private bool magnifySuppressedForScrollingActions;
+        public bool MagnifySuppressedForScrollingActions
+        {
+            get { return magnifySuppressedForScrollingActions; }
+            set { SetProperty(ref magnifySuppressedForScrollingActions, value); }
+        }
+
+        private bool debug;
+        public bool Debug
+        {
+            get { return debug; }
+            set { SetProperty(ref debug, value); }
+        }
+
+        private bool allowRepeatKeyActionsAwayFromKey;
+        public bool AllowRepeatKeyActionsAwayFromKey
+        {
+            get { return allowRepeatKeyActionsAwayFromKey; }
+            set { SetProperty(ref allowRepeatKeyActionsAwayFromKey, value); }
+        }
+
+        public bool ChangesRequireRestart
+        {
+            get
+            {
+
+                return (Settings.Default.Debug != Debug);
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void Load()
+        {
+            ShowSplashScreen = Settings.Default.ShowSplashScreen;
+            CheckForUpdates = Settings.Default.CheckForUpdates;
+            SuppressTriggerWarning = Settings.Default.SuppressTriggerWithoutPositionError;
+            MagnifySuppressedForScrollingActions = Settings.Default.MagnifySuppressedForScrollingActions;
+            Debug = Settings.Default.Debug;
+            AllowRepeatKeyActionsAwayFromKey = Settings.Default.AllowRepeatKeyActionsAwayFromKey;
+            
+        }
+
+        public void ApplyChanges()
+        {
+            Settings.Default.ShowSplashScreen = ShowSplashScreen;
+            Settings.Default.CheckForUpdates = CheckForUpdates;
+            Settings.Default.SuppressTriggerWithoutPositionError = SuppressTriggerWarning;
+            Settings.Default.MagnifySuppressedForScrollingActions = MagnifySuppressedForScrollingActions;
+            Settings.Default.Debug = Debug;
+            Settings.Default.AllowRepeatKeyActionsAwayFromKey = AllowRepeatKeyActionsAwayFromKey;
+
+        }
+
+        #endregion
+    }
+}
--- src/JuliusSweetland.OptiKey.EyeMine/UI/ViewModels/ManagementViewModelEyeMine.cs
+++ src/JuliusSweetland.OptiKey.EyeMine/UI/ViewModels/ManagementViewModelEyeMine.cs
@@ -44,6 +44,7 @@ namespace JuliusSweetland.OptiKey.UI.ViewModels
             FeaturesViewModel = new FeaturesViewModel();
             WordsViewModel = new WordsViewModel(dictionaryService);
             AboutViewModel = new AboutViewModel();
+            OtherViewModel = new OtherViewModel();
 
             //Instantiate interaction requests and commands
             ConfirmationRequest = new InteractionRequest<Confirmation>();
@@ -100,6 +101,7 @@ namespace JuliusSweetland.OptiKey.UI.ViewModels
         public FeaturesViewModel FeaturesViewModel { get; private set; }
         public WordsViewModel WordsViewModel { get; private set; }
         public AboutViewModel AboutViewModel { get; private set; }
+        public OtherViewModel OtherViewModel { get; private set; }
 
         public InteractionRequest<Confirmation> ConfirmationRequest { get; private set; }
         public DelegateCommand<Window> OkCommand { get; private set; }
@@ -221,6 +223,7 @@ namespace JuliusSweetland.OptiKey.UI.ViewModels
             VisualsViewModel.ApplyChanges();
             FeaturesViewModel.ApplyChanges();
             WordsViewModel.ApplyChanges();
+            OtherViewModel.ApplyChanges();
         }
 
         private void RestartRequest()
